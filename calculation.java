77016191613c937247b7191d6323b42b961fbeb8
nguyenduyanh <duyanh7724@gamil.com>
2024-02-29 16:14:51 +0700

    init project

---
+++ calculation.java
@@ -0,0 +1,17 @@
+public class calculation {
+    public calculation( int a, int b){
+        this.a=a;
+        this.b=b;
+
+    }
+    public int sum(){
+        return a+b;
+    }
+    public int minus(){
+        return a-b;
+    }
+    public stattic void(String [] arga){
+        calculatinon cal = new calculation (a:5, b:7);
+        System.out.println(cal.sum());
+    }
+}
---
+++ studen.java
@@ -0,0 +1,11 @@
+public class studen {
+    private String id;
+    private String name;
+    private String add;
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+}
